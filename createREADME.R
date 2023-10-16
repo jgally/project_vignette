d128c2e3176fc876bcbd2f25e51631a97cf8c956
Jasmine Gallaway <gallyjas@gmail.com>
2023-10-16 05:31:35 -0400

    Sending png of plots

--- createREADME.R
+++ createREADME.R
@@ -1,10 +1,10 @@
 #Creating README.md  
 library(rmarkdown)
-rmarkdown::render("C:/Users/nowat/OneDrive/Documents/ST558 Work/project_vignette/Project-2.Rmd",  
+rmarkdown::render("C:/Users/nowat/OneDrive/Documents/ST558 Work/project_vignette/Project2.Rmd",  
                   output_format = "github_document",
                   output_file = "README.md"
 )  
 
 #checking that the file pathway actually exists
-file.exists("C:/Users/nowat/OneDrive/Documents/ST558 Work/project_vignette/Project-2.Rmd")
+file.exists("C:/Users/nowat/OneDrive/Documents/ST558 Work/project_vignette/Project2.Rmd")
 
